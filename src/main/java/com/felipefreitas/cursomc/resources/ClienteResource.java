bfaf50a2a477c28dd8850956f26a92f10d2b33df
Felipe Ramos de Freitas <felipe.freitas@populisrh.com>
2020-05-23 16:03:49 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/felipefreitas/cursomc/resources/ClienteResource.java
@@ -0,0 +1,26 @@
+package com.felipefreitas.cursomc.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.felipefreitas.cursomc.domain.Cliente;
+import com.felipefreitas.cursomc.services.ClienteService;
+
+@RestController
+@RequestMapping(value = "/clientes")
+public class ClienteResource {
+	
+	@Autowired
+	private ClienteService service;
+	
+	@GetMapping(path = "/{id}")
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Cliente obj = service.buscar(id);
+		 return  ResponseEntity.ok().body(obj);
+	} 
+
+}
