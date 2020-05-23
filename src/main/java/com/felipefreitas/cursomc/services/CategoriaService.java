ac60c199f0eb5429570c1cc4400f971c24966213
Felipe Ramos de Freitas <felipe.freitas@populisrh.com>
2020-05-23 11:38:11 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/felipefreitas/cursomc/services/CategoriaService.java
@@ -0,0 +1,21 @@
+package com.felipefreitas.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.felipefreitas.cursomc.domain.Categoria;
+import com.felipefreitas.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null); 
+	}
+}
