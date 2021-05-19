9382d07c10824c174c7a125143ad4c3efffa5b23
Felipe Ramos de Freitas <felipe.freitas@populisrh.com>
2021-05-19 16:29:33 -0300

    Criando Profile de desenvolvimento

---
+++ src/main/java/com/felipefreitas/cursomc/config/DevConfig.java
@@ -0,0 +1,30 @@
+package com.felipefreitas.cursomc.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.felipefreitas.cursomc.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instantiateDataBase() throws Exception {
+		
+		if(! "create".equals(strategy)) {
+			return false;
+		}
+		dbService.instantiateTestDataBase();
+		return true;
+	}
+}
