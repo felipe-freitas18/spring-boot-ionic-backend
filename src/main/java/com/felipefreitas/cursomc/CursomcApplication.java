a80bd6f132fb3332f8b846c52a2e42c64232601a
Felipe Ramos de Freitas <felipe.freitas@populisrh.com>
2022-08-18 15:58:26 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/felipefreitas/cursomc/CursomcApplication.java
+++ src/main/java/com/felipefreitas/cursomc/CursomcApplication.java
@@ -1,46 +1,25 @@
 package com.felipefreitas.cursomc;
 
-import java.text.SimpleDateFormat;
-import java.util.Arrays;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.felipefreitas.cursomc.domain.Categoria;
-import com.felipefreitas.cursomc.domain.Cidade;
-import com.felipefreitas.cursomc.domain.Cliente;
-import com.felipefreitas.cursomc.domain.Endereco;
-import com.felipefreitas.cursomc.domain.Estado;
-import com.felipefreitas.cursomc.domain.ItemPedido;
-import com.felipefreitas.cursomc.domain.Pagamento;
-import com.felipefreitas.cursomc.domain.PagamentoComBoleto;
-import com.felipefreitas.cursomc.domain.PagamentoComCartao;
-import com.felipefreitas.cursomc.domain.Pedido;
-import com.felipefreitas.cursomc.domain.Produto;
-import com.felipefreitas.cursomc.domain.enums.EstadoPagamento;
-import com.felipefreitas.cursomc.domain.enums.TipoCliente;
-import com.felipefreitas.cursomc.repositories.CategoriaRepository;
-import com.felipefreitas.cursomc.repositories.CidadeRepository;
-import com.felipefreitas.cursomc.repositories.ClienteRepository;
-import com.felipefreitas.cursomc.repositories.EnderecoRepository;
-import com.felipefreitas.cursomc.repositories.EstadoRepository;
-import com.felipefreitas.cursomc.repositories.ItemPedidoRepository;
-import com.felipefreitas.cursomc.repositories.PagamentoRepository;
-import com.felipefreitas.cursomc.repositories.PedidoRepository;
-import com.felipefreitas.cursomc.repositories.ProdutoRepository;
+import com.felipefreitas.cursomc.services.S3Service;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
+	@Autowired
+	private S3Service s3Service;
+	
  	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+		s3Service.uploadFile("C:\\Users\\Felipe\\Desktop\\cliente_1.jpg");
 	}
 
 }
