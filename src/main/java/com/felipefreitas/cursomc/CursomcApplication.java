9348435ba1ccdc73811d8b81e4c152eeffed2c20
Felipe Ramos de Freitas <felipe.freitas@populisrh.com>
2022-08-18 18:46:52 -0300

    Enviando imagem via endpoint

--- src/main/java/com/felipefreitas/cursomc/CursomcApplication.java
+++ src/main/java/com/felipefreitas/cursomc/CursomcApplication.java
@@ -1,25 +1,18 @@
 package com.felipefreitas.cursomc;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.felipefreitas.cursomc.services.S3Service;
-
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
-	@Autowired
-	private S3Service s3Service;
-	
  	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		s3Service.uploadFile("C:\\Users\\Felipe\\Desktop\\cliente_1.jpg");
 	}
 
 }
--- src/main/java/com/felipefreitas/cursomc/services/ClienteService.java
+++ src/main/java/com/felipefreitas/cursomc/services/ClienteService.java
@@ -1,5 +1,6 @@
 package com.felipefreitas.cursomc.services;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -11,6 +12,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.felipefreitas.cursomc.domain.Cidade;
 import com.felipefreitas.cursomc.domain.Cliente;
@@ -37,6 +39,9 @@ public class ClienteService {
 
 	@Autowired
 	private EnderecoRepository enderecoRepo;
+	
+	@Autowired
+	private S3Service s3Service;
 	
 	public Cliente find(Integer id) {
 		UserSS user = UserService.authenticated();
@@ -99,6 +104,10 @@ public class ClienteService {
 		repo.save(obj);
 		enderecoRepo.saveAll(obj.getEnderecos());
 		return obj;
+	}
+	
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		return s3Service.uploadFile(multipartFile);
 	}
 
 }
