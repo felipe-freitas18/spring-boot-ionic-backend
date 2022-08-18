d746c293ef7dadd9178fc4b8a72e08353e0dda64
Felipe Ramos de Freitas <felipe.freitas@populisrh.com>
2022-08-18 09:11:35 -0300

    Restrição de conteúdo: cliente só recupera seus pedidos

--- src/main/java/com/felipefreitas/cursomc/services/PedidoService.java
+++ src/main/java/com/felipefreitas/cursomc/services/PedidoService.java
@@ -6,8 +6,12 @@ import java.util.Optional;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.felipefreitas.cursomc.domain.Cliente;
 import com.felipefreitas.cursomc.domain.ItemPedido;
 import com.felipefreitas.cursomc.domain.PagamentoComBoleto;
 import com.felipefreitas.cursomc.domain.Pedido;
@@ -15,6 +19,8 @@ import com.felipefreitas.cursomc.domain.enums.EstadoPagamento;
 import com.felipefreitas.cursomc.repositories.ItemPedidoRepository;
 import com.felipefreitas.cursomc.repositories.PagamentoRepository;
 import com.felipefreitas.cursomc.repositories.PedidoRepository;
+import com.felipefreitas.cursomc.security.UserSS;
+import com.felipefreitas.cursomc.services.exceptions.AuthorizationException;
 import com.felipefreitas.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -69,5 +75,15 @@ public class PedidoService {
 		
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	}
+	
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		UserSS user = UserService.authenticated();
+		if(user == null) {
+			throw new AuthorizationException("Acesso Negado");
+		}
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.find(user.getId());
+		return repo.findByCliente(cliente, pageRequest);
 	}
 }
